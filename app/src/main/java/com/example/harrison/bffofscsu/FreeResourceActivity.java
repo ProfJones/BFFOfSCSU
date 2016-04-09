39516d227bb9d4646e7ec0d8ae63c9793cc9ee92
ProfJones <harrisongsmith@live.com>
2016-04-09 08:02:00 -0500

    Added A list for free activites

--- app/src/main/java/com/example/harrison/bffofscsu/FreeResourceActivity.java
+++ app/src/main/java/com/example/harrison/bffofscsu/FreeResourceActivity.java
@@ -1,13 +1,78 @@
 package com.example.harrison.bffofscsu;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class FreeResourceActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String[] myStringArray = {"Free Parking", "Free Open Skate", "Free ponies", "Free dom!"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, myStringArray);
+
         setContentView(R.layout.free_resource_layout);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "FreeResource Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.harrison.bffofscsu/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "FreeResource Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.harrison.bffofscsu/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
--- app/src/main/java/com/example/harrison/bffofscsu/MainActivity.java
+++ app/src/main/java/com/example/harrison/bffofscsu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.harrison.bffofscsu;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,9 +10,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -44,32 +60,76 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        switch (id){
+        switch (id) {
             case R.id.office_hour:
-                Toast.makeText(this,"Touched office hour",Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(getApplicationContext(),OfficeHourActivity.class);
-                intent.putExtra("key",1);
+                Toast.makeText(this, "Touched office hour", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), OfficeHourActivity.class);
+                intent.putExtra("key", 1);
                 startActivity(intent);
                 return true;
             case R.id.free_resource:
-                Toast.makeText(this,"Touched free resource",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Touched free resource", Toast.LENGTH_LONG).show();
+                Intent freeResourceIntent = new Intent(getApplicationContext(), FreeResourceActivity.class);
+
+                freeResourceIntent.putExtra("key", 1);
+                startActivity(freeResourceIntent);
                 return true;
             case R.id.event_news:
-                Toast.makeText(this,"Touched event news",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Touched event news", Toast.LENGTH_LONG).show();
                 return true;
             case R.id.me:
-                Toast.makeText(this,"Touched profile",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Touched profile", Toast.LENGTH_LONG).show();
                 return true;
             case R.id.search:
-                Toast.makeText(this,"Touched search",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Touched search", Toast.LENGTH_LONG).show();
                 return true;
             case R.id.action_settings:
-                Toast.makeText(this,"Touched setting",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Touched setting", Toast.LENGTH_LONG).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
 
         }
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.harrison.bffofscsu/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.harrison.bffofscsu/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
